9913e84789d91cf21b04194ac6741b2843cdebfd
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-10 10:56:29 -0500

    Types options dropdown outputting to HTML

--- scripts/type.js
+++ scripts/type.js
@@ -1,3 +1,13 @@
-export const getType = async () => {
-  const response = await fetch("http://localhost:8099/");
+export const getTypes = async () => {
+  const response = await fetch("http://localhost:8088/types");
+  const types = await response.json();
+  let html = `<h3> Choose your vehicle type</h3>
+                <select id="type_choice">
+                    <option value="0"> Choose a vehicle type... </option>`;
+  const typeArray = types.map((type) => {
+    return `<option value="${type.id}"> ${type.description} </option>`;
+  });
+  html += typeArray.join("");
+  html += `</select>`;
+  return html;
 };
