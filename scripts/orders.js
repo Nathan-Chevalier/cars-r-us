dd46c839a8bfe2c05cde92de6ec44c882fe8ed30
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-10 11:11:43 -0500

    Types functioning correctly

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,17 +1,22 @@
 export const getOrders = async () => {
   const response = await fetch(
-    "http://localhost:8088/orders?_expand=color&_expand=interior&_expand=package&_expand=wheels"
+    "http://localhost:8088/orders?_expand=color&_expand=interior&_expand=package&_expand=wheels&_expand=type"
   );
   const orders = await response.json();
 
   let html = `<h2>Current Orders</h2>`;
   const ordersArray = orders.map((order) => {
-    const orderPrice =
+    let orderPrice =
       order.color.price +
       order.interior.price +
       order.package.price +
       order.wheels.price;
-    return `<div class="order"> Order #${order.id} costs $${orderPrice}</div>`;
+    orderPrice = orderPrice * order.type.multiplier;
+    let convertedPrice = orderPrice.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    return `<div class="order"> Order #${order.id} costs ${convertedPrice}</div>`;
   });
   html += ordersArray.join("");
 
--- scripts/type.js
+++ scripts/type.js
@@ -1,4 +1,7 @@
+import { setType } from "./transient.js";
+
 export const getTypes = async () => {
+  document.addEventListener("change", typeChange);
   const response = await fetch("http://localhost:8088/types");
   const types = await response.json();
   let html = `<h3> Choose your vehicle type</h3>
@@ -10,4 +13,11 @@ export const getTypes = async () => {
   html += typeArray.join("");
   html += `</select>`;
   return html;
+};
+
+const typeChange = (changeEvent) => {
+  if (changeEvent.target.id === "type_choice") {
+    const converted = parseInt(changeEvent.target.value);
+    setType(converted);
+  }
 };
